78f0dd1ed2fdceeb40739751fd8bff6932442c2e
Arun K Bharathan <arunkbharathan@gmail.com>
2020-05-23 00:14:46 +0530

    removed docs

--- io/InMemoryPlayerStore.go
+++ io/InMemoryPlayerStore.go
@@ -1,16 +1,13 @@
 package main
 
-// NewInMemoryPlayerStore initialises an empty player store
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
 	return &InMemoryPlayerStore{map[string]int{}}
 }
 
-// InMemoryPlayerStore collects data about players in memory
 type InMemoryPlayerStore struct {
 	store map[string]int
 }
 
-// GetLeague returns a collection of Players
 func (i *InMemoryPlayerStore) GetLeague() []Player {
 	var league []Player
 	for name, wins := range i.store {
@@ -19,12 +16,10 @@ func (i *InMemoryPlayerStore) GetLeague() []Player {
 	return league
 }
 
-// RecordWin will record a player's win
 func (i *InMemoryPlayerStore) RecordWin(name string) {
 	i.store[name]++
 }
 
-// GetPlayerScore retrieves scores for a given player
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
 	return i.store[name]
 }
