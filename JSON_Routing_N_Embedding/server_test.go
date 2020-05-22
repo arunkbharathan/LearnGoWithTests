6eb10376a2b8daadbd01aad91ecc9639203e6a52
Arun K Bharathan <arunkbharathan@gmail.com>
2020-05-22 17:00:50 +0530

    first test

--- JSON_Routing_N_Embedding/server_test.go
+++ JSON_Routing_N_Embedding/server_test.go
@@ -111,3 +111,17 @@ func assertResponseBody(t *testing.T, got, want string) {
 		t.Errorf("response body is wrong, got %q want %q", got, want)
 	}
 }
+
+func TestLeague(t *testing.T) {
+	store := StubPlayerStore{}
+	server := &PlayerServer{&store}
+
+	t.Run("it returns 200 on /league", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+}
