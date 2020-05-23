5840fa79a9af04c35e49b8e8ead7c30f173640ea
Arun K Bharathan <arunkbharathan@gmail.com>
2020-05-23 11:10:49 +0530

    cp

--- io/FileSystemStore.go
+++ io/FileSystemStore.go
@@ -14,27 +14,25 @@ func (f *FileSystemPlayerStore) GetLeague() League {
 	league, _ := NewLeague(f.database)
 	return league
 }
-
 func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
-	var wins int
 
-	for _, player := range f.GetLeague() {
-		if player.Name == name {
-			wins = player.Wins
-			break
-		}
+	player := f.GetLeague().Find(name)
+
+	if player != nil {
+		return player.Wins
 	}
 
-	return wins
+	return 0
 }
 
 func (f *FileSystemPlayerStore) RecordWin(name string) {
 	league := f.GetLeague()
+	player := league.Find(name)
 
-	for i, player := range league {
-		if player.Name == name {
-			league[i].Wins++
-		}
+	if player != nil {
+		player.Wins++
+	} else {
+		league = append(league, Player{name, 1})
 	}
 
 	f.database.Seek(0, 0)
